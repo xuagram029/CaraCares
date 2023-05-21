4dbab9399d9c1effc9168739b4ffa513c0e8151a
Margaux <margauxgalvez01@gmail.com>
2023-05-21 16:04:50 +0800

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,9 +7,7 @@ import { BiLogOut } from "react-icons/bi";
 import { MdPets } from "react-icons/md";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { BiFileFind } from "react-icons/bi";
-
-
-
+import { BsPencilSquare } from "react-icons/bs";
 
 import { AiOutlineSetting } from "react-icons/ai";
 
@@ -17,6 +15,7 @@ const Sidebar = () => {
     const [open, setOpen] = useState(true)
     const [openMenu, setOpenMenu] = useState(false)
     const [openLf, setOpenLf] = useState(false)
+    const [openPe, setOpenPe] = useState(false)
 
   return (
     <div className={`flex flex-col justify-between bg-slate-400 h-screen p-6  relative ${open ? "w-72": "w-20" } duration-500`}>
@@ -26,57 +25,69 @@ const Sidebar = () => {
 
             <div className={`pt-6 flex items-center ${!open ? "border-none" : "border-b border-red-300"}`}>
                 <RxDashboard
-                    className={`text-xl cursor-pointer min-w-fit`} />
-                    <h1 className={`origin-left ml-5 text-lg cursor-pointer ${!open ? "scale-0 duration-300" : "duration-500"}`}>Dashboard</h1>
+                    className={`text-lg cursor-pointer min-w-fit`} />
+                    <h1 className={`origin-left ml-5 text-base cursor-pointer ${!open ? "scale-0 duration-300" : "duration-500"}`}>Dashboard</h1>
             </div>
 
             <div className={`pt-6 flex items-center ${!open ? "border-none" : "border-b border-red-300"}`}>
                 <HiOutlineUsers
-                    className={`text-xl cursor-pointer min-w-fit`} />
-                    <h1 className={`origin-left ml-5 text-lg cursor-pointer ${!open ? "scale-0 duration-300" : "duration-500"}`}>Users</h1>
-            </div>
-
-            <div className={`pt-6 flex items-center ${!open ? "border-none" : "border-b border-red-300"}`}>
-                <AiOutlineSetting
-                    className={`text-xl cursor-pointer min-w-fit`}/>
-                    <h1 className={`origin-left ml-5 text-lg cursor-pointer ${!open ? "scale-0 duration-300" : "duration-500"}`}>Settings</h1>
+                    className={`text-lg cursor-pointer min-w-fit`} />
+                    <h1 className={`origin-left ml-5 text-base cursor-pointer ${!open ? "scale-0 duration-300" : "duration-500"}`}>Users</h1>
             </div>
 
             <div className='pt-6 flex flex-col items-center'>
                 <div className={`flex self-start border-b border-red-300 w-full ${!open ? "border-none" : "border-b border-red-300"}`}>
                     <MdPets
-                        className={`text-xl cursor-pointer min-w-fit`}/>
-                        <h1 className={`origin-left ml-5 text-lg cursor-pointer ${!open ? "hidden duration-300" : "block duration-500"}`} onClick={() => {setOpenMenu(!openMenu)}}>Foster / Adoption</h1>
+                        className={`text-lg cursor-pointer min-w-fit`}/>
+                        <h1 className={`origin-left ml-5 text-base cursor-pointer ${!open ? "hidden duration-300" : "block duration-500"}`} onClick={() => {setOpenMenu(!openMenu)}}>Foster / Adoption</h1>
                         <AiOutlineArrowDown
-                        className={`text-xl cursor-pointer absolute right-1 ${!openMenu ? "rotate-180 duration-300" : "duration-500"} ${!open ? "hidden": "visible"}`} onClick={() => {setOpenMenu(!openMenu)}}/>
+                        className={`text-lg cursor-pointer absolute right-1 ${!openMenu ? "rotate-180 duration-300" : "duration-500"} ${!open ? "hidden": "visible"}`} onClick={() => {setOpenMenu(!openMenu)}}/>
                 </div>
                 { openMenu && <div className={`pt-4 w-full self-start ${!open && "hidden"}`}>
-                    <div className='text-lg border-b border-red-300 cursor-pointer'>Adoption</div>
-                    <div className='text-lg mt-2  border-b border-red-300 cursor-pointer'>Foster</div>
+                    <div className='text-sm border-b border-red-300 cursor-pointer'>Adoption</div>
+                    <div className='text-sm mt-2  border-b border-red-300 cursor-pointer'>Foster</div>
                 </div> }
             </div>
 
             <div className='pt-6 flex flex-col items-center'>
                 <div className={`flex self-start border-b border-red-300 w-full ${!open ? "border-none" : "border-b border-red-300"}`}>
                     <BiFileFind
-                        className={`text-xl cursor-pointer min-w-fit`}/>
-                        <h1 className={`origin-left ml-5 text-lg cursor-pointer ${!open ? "hidden duration-300" : "block duration-500"}`} onClick={() => {setOpenLf(!openLf)}}>Lost & Found</h1>
+                        className={`text-lg cursor-pointer min-w-fit`}/>
+                        <h1 className={`origin-left ml-5 text-base cursor-pointer ${!open ? "hidden duration-300" : "block duration-500"}`} onClick={() => {setOpenLf(!openLf)}}>Lost & Found</h1>
                         <AiOutlineArrowDown
-                        className={`text-xl cursor-pointer absolute right-1 ${!openLf ? "rotate-180 duration-300" : "duration-500"} ${!open ? "hidden": "visible"}`} onClick={() => {setOpenLf(!openLf)}}/>
+                        className={`text-lg cursor-pointer absolute right-1 ${!openLf ? "rotate-180 duration-300" : "duration-500"} ${!open ? "hidden": "visible"}`} onClick={() => {setOpenLf(!openLf)}}/>
                 </div>
                 { openLf && <div className={`pt-4 w-full self-start ${!open && "hidden"}`}>
-                    <div className='text-lg border-b border-red-300 cursor-pointer'>Lost Pets</div>
-                    <div className='text-lg mt-2  border-b border-red-300 cursor-pointer'>Found Pets</div>
+                    <div className='text-sm border-b border-red-300 cursor-pointer'>Lost Pets</div>
+                    <div className='text-sm mt-2  border-b border-red-300 cursor-pointer'>Found Pets</div>
                 </div> }
             </div>
 
+            <div className='pt-6 flex flex-col items-center'>
+                <div className={`flex self-start border-b border-red-300 w-full ${!open ? "border-none" : "border-b border-red-300"}`}>
+                    <BsPencilSquare
+                        className={`text-lg cursor-pointer min-w-fit`}/>
+                        <h1 className={`origin-left ml-5 text-base cursor-pointer ${!open ? "hidden duration-300" : "block duration-500"}`} onClick={() => {setOpenPe(!openPe)}}>Panel Encode</h1>
+                        <AiOutlineArrowDown
+                        className={`text-lg cursor-pointer absolute right-1 ${!openPe ? "rotate-180 duration-300" : "duration-500"} ${!open ? "hidden": "visible"}`} onClick={() => {setOpenPe(!openPe)}}/>
+                </div>
+                { openPe && <div className={`pt-4 w-full self-start ${!open && "hidden"}`}>
+                    <div className='text-sm border-b border-red-300 cursor-pointer'>Lost Reports</div>
+                    <div className='text-sm mt-2  border-b border-red-300 cursor-pointer'>Found Reports</div>
+                </div> }
+            </div>
 
+            <div className={`pt-6 flex items-center ${!open ? "border-none" : "border-b border-red-300"}`}>
+                <AiOutlineSetting
+                    className={`text-lg cursor-pointer min-w-fit`}/>
+                    <h1 className={`origin-left ml-5 text-base cursor-pointer ${!open ? "scale-0 duration-300" : "duration-500"}`}>Settings</h1>
+            </div>
 
         </div>
             <div className='flex items-center'>
                 <BiLogOut
-                className={`text-xl cursor-pointer min-w-fit`} />
-                <h1 className={`origin-left ml-5 text-lg cursor-pointer ${!open ? "scale-0 duration-300" : "duration-500"}`}>Logout</h1>
+                className={`text-lg cursor-pointer min-w-fit`} />
+                <h1 className={`origin-left ml-5 text-base cursor-pointer ${!open ? "scale-0 duration-300" : "duration-500"}`}>Logout</h1>
             </div>
     </div>
   )
