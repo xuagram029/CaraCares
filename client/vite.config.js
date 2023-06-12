49316ecd357a6d4a1d24df7c08448138859de5dd
Margaux <margauxgalvez01@gmail.com>
2023-06-13 01:48:03 +0800

    deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     hmr: {
       overlay: false,
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['styled-components'],
     },
   },
 });
