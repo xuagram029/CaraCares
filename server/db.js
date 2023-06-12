47a665fc7aa0c48921862ef423237a73908f33e9
Margaux <margauxgalvez01@gmail.com>
2023-06-13 01:04:21 +0800

    db error handler

--- server/db.js
+++ server/db.js
@@ -7,4 +7,16 @@ const db = mysql.createConnection({
     database:"cara_db",
 })
 
+db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err);
+      return;
+    }
+    console.log("Connected to the database!");
+  });
+  
+  db.on("error", (err) => {
+    console.error("Database connection error:", err);
+});
+
 module.exports = db
